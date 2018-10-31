54118ab0faac59660446a7f7a574259f14300bff
Chan Lee <chanethanlee@gmail.com>
2018-10-30 19:39:02 -0700

    use angularfire object method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase} from 'angularfire2/database';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
@@ -8,9 +8,21 @@ import { Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
   courses$;
+  course$;
+  // subscription: Subscription;
+
   constructor (db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses');
-  }
+    this.courses$ = db.list('/courses').valueChanges();
+    this.course$ = db.object('/courses/0').valueChanges();
+  //   this.subscription = db.list('/courses').valueChanges().subscribe(courses => {
+  //       this.courses =  courses;
+  //       console.log(courses);
+  //     });
+  // }
+
+  // ngOnDestroy() {
+  //   this.subscription.unsubscribe();
+  // }
 }
