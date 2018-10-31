11b1819c1aa179deab39fc1baac97cd066af852a
Chan Lee <chanethanlee@gmail.com>
2018-10-30 21:32:20 -0700

    add push to list with created object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AngularFireDatabase} from 'angularfire2/database';
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 
 @Component({
@@ -9,15 +9,18 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses$;
+  courses;
+  courses$: Observable<{}[]>;
   course$;
   author$;
   // subscription: Subscription;
 
   constructor (db: AngularFireDatabase) {
-    this.courses$ = db.list('/courses').valueChanges();
+    this.courses = db.list('/courses')
+    this.courses$ = this.courses.valueChanges();
     this.course$ = db.object('/courses/0').valueChanges();
     this.author$ = db.object('/authors/0').valueChanges();
+    console.log(this.courses$);
   //   this.subscription = db.list('/courses').valueChanges().subscribe(courses => {
   //       this.courses =  courses;
   //       console.log(courses);
@@ -27,5 +30,23 @@ export class AppComponent {
   // ngOnDestroy() {
   //   this.subscription.unsubscribe();
   // }
+  }
+  add(course: HTMLInputElement) { 
+    // this.courses.push(course.value);
+
+    //noSql, can push complex object.
+    this.courses.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        {title: "comp1"},
+        {title: "comp2"},
+        {title: "comp3"},
+      ],
+      });
+    course.value = '';
+  }
 }
-}
+
+//https://github.com/angular/angularfire2/issues/1158
