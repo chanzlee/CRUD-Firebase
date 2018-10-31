2c7130af0f1258d517f9cf98db008622639fe868
Chan Lee <chanethanlee@gmail.com>
2018-10-30 18:05:13 -0700

    initialize files with firebase configs.

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyCnvAXR4ejfOChkgg3Afs-5MendPKyp9FE",
+  authDomain: "fir-demo-49a0d.firebaseapp.com",
+  databaseURL: "https://fir-demo-49a0d.firebaseio.com",
+  projectId: "fir-demo-49a0d",
+  storageBucket: "",
+  messagingSenderId: "943059090871"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
